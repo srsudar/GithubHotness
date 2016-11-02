f381d1679d03f031033ad86644d35b64f3fd9396
Sam Sudar <sudar.sam@gmail.com>
2016-11-02 09:18:13 -0700

    Start adding tests.
    Test not running for some reason, maybe a version issue?

--- app/src/test/java/org/cse390/githubhotness/ui/activity/RepoListActivityTest.java
+++ app/src/test/java/org/cse390/githubhotness/ui/activity/RepoListActivityTest.java
@@ -1,8 +1,30 @@
 package org.cse390.githubhotness.ui.activity;
 
+import org.cse390.githubhotness.BuildConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Created by sudars on 11/1/16.
  */
-
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 22)
 public class RepoListActivityTest {
+  RepoListActivity activity;
+
+  @Before
+  public void before() {
+//    activity = Robolectric.setupActivity(RepoListActivity.class);
+  }
+
+  @Test
+  public void notNull() {
+    assertThat(activity).isNotNull();
+  }
 }
